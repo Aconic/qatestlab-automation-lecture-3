088db5bc593f176e7d93c9b8189baadf6d97858e
Aconic <tygreman@gmail.com>
2017-04-14 17:11:16 +0300

    Category test performed

--- src/main/java/myprojects/automation/assignment3/tests/CreateCategoryTest.java
+++ src/main/java/myprojects/automation/assignment3/tests/CreateCategoryTest.java
@@ -1,19 +1,54 @@
 package myprojects.automation.assignment3.tests;
 
 import myprojects.automation.assignment3.BaseScript;
+import myprojects.automation.assignment3.GeneralActions;
+import myprojects.automation.assignment3.utils.Properties;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
-public class CreateCategoryTest extends BaseScript {
-    public static void main(String[] args) throws InterruptedException {
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class CreateCategoryTest extends BaseScript
+{
+    public static void main(String[] args) throws InterruptedException
+    {
         // TODO prepare driver object
-        // WebDriver driver = getConfiguredDriver();
-        // ...
+
+        EventFiringWebDriver driver = new EventFiringWebDriver(getDriver());
+        String testCategName= "Test Category";
+        driver.register(new EventHandler());
+
+        GeneralActions actions = new GeneralActions(driver);
+
+        driver.get(Properties.getBaseAdminUrl());
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         // login
+        actions.login("webinar.test@gmail.com", "Xcg7299bnSmMuRLp9ITw");
 
         // create category
+        actions.createCategory(testCategName);
 
         // check that new category appears in Categories table
+        WebElement filterName = driver.findElement(By.xpath("//input[@name='categoryFilter_name']"));
+        filterName.sendKeys(testCategName);
+        filterName.sendKeys(Keys.ENTER);
+        actions.waitForContentLoad();
+
+        List<WebElement> categories = driver.findElements(By.xpath("//tr//td[@class='pointer'][1]"));
+         for (WebElement element : categories)
+        {
+            if (element.getText().equals(testCategName))
+            {
+                System.out.println("New category is displayed on the list");
+            }
+        }
 
         // finish script
+        driver.close();
     }
 }
